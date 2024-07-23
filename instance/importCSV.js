ec7a8cea642f14039da5b9292b2ac3e7ba596c49
aptopspeed <maplestory.ssso@gmail.com>
2024-07-23 15:23:09 +0700

    create emission data into collection

--- instance/importCSV.js
+++ instance/importCSV.js
@@ -1,5 +1,7 @@
 // const ObjectId = mongoose.Types.ObjectId;
 
+const Emission = require("../models/Emission")
+
 // const importCSV = async () => {
 //   const countriesMap = await Country.find().select('shortName _id').then(countries => {
 //     const map = {};
@@ -45,3 +47,56 @@
 // };
 
 // importCSV();
+
+
+
+//// Import CSV Emissions
+// async function importCSV(filePath) {
+//     // Create mappings for countries and sectors
+//     const countriesMap = await Country.find().select('shortName _id').then(countries => {
+//       const map = {};
+//       countries.forEach(country => {
+//         map[country.shortName] = country._id;
+//       });
+//       return map;
+//     });
+  
+//     const sectorsMap = await Sector.find().select('sectorName _id').then(sectors => {
+//       const map = {};
+//       sectors.forEach(sector => {
+//         map[sector.sectorName] = sector._id;
+//       });
+//       return map;
+//     });
+  
+//     const results = [];
+//     fs.createReadStream(filePath)
+//       .pipe(csv())
+//       .on('data', (data) => {
+//         const countryId = countriesMap[data.shortName];
+//         const sectorId = sectorsMap[data.sector];
+  
+//         if (countryId && sectorId) {
+//           results.push({
+//             countryId,
+//             sectorId,
+//             year: Number(data.year),
+//             emissions: {
+//               CO2: parseFloat(data.CO2) || 0,
+//               FGas: parseFloat(data.FGas) || 0,
+//               N2O: parseFloat(data.N2O) || 0,
+//               CH4: parseFloat(data.CH4) || 0
+//             }
+//           });
+//         }
+//       })
+//       .on('end', async () => {
+//         try {
+//           await Emission.insertMany(results);
+//           console.log('CSV data successfully imported into MongoDB.');
+//         } catch (error) {
+//           console.error('Error importing data:', error);
+//         }
+//       });
+//   }
+// await importCSV("ghg-emissions-dataset.csv")
--- models/Emission.js
+++ models/Emission.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
 
 const emissionSchema = new mongoose.Schema({
-    countryId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Country', 
-        required: true 
+  countryId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Country', 
+    required: true 
     },
-    sectorId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Sector', 
-        required: true 
-    },
-    year: { 
-        type: Number, 
-        required: true 
-    },
-    emissions: { 
-        type: Number, 
-        required: true 
-    }
+  sectorId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Sector', 
+    required: true },
+  year: { 
+    type: Number, 
+    required: true },
+  emissions: {
+    CO2: { type: Number, required: true },
+    FGas: { type: Number, required: true },
+    N2O: { type: Number, required: true },
+    CH4: { type: Number, required: true },
+  }
 });
 
 module.exports = mongoose.model('Emission', emissionSchema);
